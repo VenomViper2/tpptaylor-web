2e69fd534b2693df9435f3aada5c71dfd5f8083b
Venom <tomptaylor92@googlemail.com>
2023-06-15 00:59:39 +0100

    github pages added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,6 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
   site: 'https://venomviper2.github.io/',
   base: '/my-repo',
 });
