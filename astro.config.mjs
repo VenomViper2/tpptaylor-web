3b03f9dfc8441179c856c8f76a3d2c06048babd6
Venom <tomptaylor92@googlemail.com>
2023-06-15 16:03:52 +0100

    github pages added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-  site: 'https://venomviper2.github.io'});
+  site: 'https://venomviper2.github.io',
+  base: '/tpptaylor-web',
+});
