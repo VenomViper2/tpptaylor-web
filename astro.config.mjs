947e8a4e75f8f3fcc65bfa0bd62b3818299d77de
Venom <tomptaylor92@googlemail.com>
2023-06-18 23:51:36 +0100

    added robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   build: {
     assets: 'astro'
-  }
+  },
+  site: 'https://ttaylor.com',
+  integrations: [sitemap()]
 });
